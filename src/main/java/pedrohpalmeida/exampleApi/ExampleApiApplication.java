044f601e40339404672788428bec891aef016aa1
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-01-20 21:14:28 -0300

    feat: projeto inicial

---
+++ src/main/java/pedrohpalmeida/exampleApi/ExampleApiApplication.java
@@ -0,0 +1,13 @@
+package pedrohpalmeida.exampleApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApiApplication.class, args);
+	}
+
+}
